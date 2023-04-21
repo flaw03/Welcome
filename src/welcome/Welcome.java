02abcd031b85aa341042853fc59d02e212d3c0ff
flaw03 <100543813+flaw03@users.noreply.github.com>
2023-04-21 15:04:47 +0200

    WelcomeNom

--- src/welcome/Welcome.java
+++ src/welcome/Welcome.java
@@ -1,4 +1,8 @@
 package welcome;
 
 public class Welcome {
+
+    public static String welcome(String nom) {
+        return "Hello," + nom;
+    }
 }
--- test/welcome/WelcomeTest.java
+++ test/welcome/WelcomeTest.java
@@ -2,6 +2,15 @@ package welcome;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class WelcomeTest {
+
+    @Test
+    void welcomeNom(){
+        assertEquals(Welcome.welcome("Assane"),"Hello,Assane");
+        assertEquals(Welcome.welcome("Noah"),"Hello,Noah");
+        assertEquals(Welcome.welcome("Jeremy"),"Hello,Jeremy");
+    }
 
 }
