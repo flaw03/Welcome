f206a66479162da22e96cad344b8cd302f7608ca
flaw03 <100543813+flaw03@users.noreply.github.com>
2023-04-21 15:17:49 +0200

    Exo 2 : chaine vide et null

--- src/welcome/Welcome.java
+++ src/welcome/Welcome.java
@@ -1,8 +1,17 @@
 package welcome;
 
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Trim;
+
 public class Welcome {
 
-    public static String welcome(String nom) {
-        return "Hello," + nom;
+    public static String welcome(String nom) {;
+        StringBuilder msg = new StringBuilder("Hello, ");
+        if(nom == null || nom.trim().equals("")){
+            msg.append("my friend");
+        }
+        else{
+            msg.append(nom);
+        }
+        return msg.toString();
     }
 }
--- test/welcome/WelcomeTest.java
+++ test/welcome/WelcomeTest.java
@@ -8,9 +8,15 @@ class WelcomeTest {
 
     @Test
     void welcomeNom(){
-        assertEquals(Welcome.welcome("Assane"),"Hello,Assane");
-        assertEquals(Welcome.welcome("Noah"),"Hello,Noah");
-        assertEquals(Welcome.welcome("Jeremy"),"Hello,Jeremy");
+        assertEquals(Welcome.welcome("Assane"),"Hello, Assane");
+        assertEquals(Welcome.welcome("Noah"),"Hello, Noah");
+        assertEquals(Welcome.welcome("Jeremy"),"Hello, Jeremy");
     }
 
+    @Test
+    void WelcomeChaineVide(){
+        assertEquals(Welcome.welcome(""),"Hello, my friend");
+        assertEquals(Welcome.welcome(" "),"Hello, my friend");
+        assertEquals(Welcome.welcome(null),"Hello, my friend");
+    }
 }
