8c1d4a191e7fca9eaf3e82dd5d88dc3747c10df2
flaw03 <100543813+flaw03@users.noreply.github.com>
2023-04-24 21:06:57 +0200

    EX0 7 : ajout d'"and" au dernier nom et ajour de la ponctuation

--- src/welcome/Welcome.java
+++ src/welcome/Welcome.java
@@ -9,37 +9,79 @@ public class Welcome {
         if(nom == null || nom.trim().equals("")){
             msgMinuscule.append(", my friend");
         } else{
-            trouveNoms(msgMinuscule,msgMajuscule ,nom);
+            trierNoms(msgMinuscule,msgMajuscule ,nom);
         }
+        ajouterPonctuation(msgMinuscule,msgMajuscule);
         return phrase(msgMinuscule, msgMajuscule);
     }
 
     private static String phrase(StringBuilder msgMinuscule, StringBuilder msgMajuscule) {
-        if (msgMinuscule.length() <= 5 ){
+        if (msgMinuscule.toString().equals("Hello.") ){
             return msgMajuscule.toString();
-        } else if (msgMajuscule.length() <= 7 ) {
+        } else if (msgMajuscule.toString().equals("HELLO !")) {
             return msgMinuscule.toString();
         }else{
-            return msgMinuscule + ". AND " + msgMajuscule;
+            return msgMinuscule + " AND " + msgMajuscule;
         }
     }
 
 
     private static void ajouterNoms(StringBuilder msg , String nom){
             msg.append(", ");
-            msg.append(nom.substring(0, 1).toUpperCase()).append(nom.substring(1));
+            msg.append(nomMajuscule(nom));
     }
 
+    private static String nomMajuscule(String nom){
+        return  nom.substring(0, 1).toUpperCase() + nom.substring(1) ;
+    }
 
-    private static void trouveNoms(StringBuilder msgMinuscule ,StringBuilder msgMajuscule, String chainedenom ){
-        String[] noms = chainedenom.split(",");
-        for (String nom : noms) {
-            if (nom.toUpperCase().equals(nom)) {
-                ajouterNoms(msgMajuscule, nom);
-            } else {
-                ajouterNoms(msgMinuscule, nom);
-            }
-        }
+    private static void ajouterPonctuation(StringBuilder msgMinuscule, StringBuilder msgMajuscule) {
+        msgMinuscule.append(".");
         msgMajuscule.append(" !");
     }
+
+    private static void trierNoms(StringBuilder msgMinuscule, StringBuilder msgMajuscule, String chainedenom) {
+        String[] noms = chainedenom.split(",");
+        String[] nomMaj = new String[10];
+        String[] nomMin = new String[10];
+        int nbMaj = 0;
+        int nbMin = 0;
+        for (String nom : noms) {
+            if (nom.toUpperCase().equals(nom)) {
+                nomMaj[nbMaj] = nom;
+                nbMaj++;
+            } else {
+                nomMin[nbMin] = nom;
+                nbMin++;
+            }
+        }
+        concatenerNoms(msgMajuscule, nomMaj, nbMaj);
+        concatenerNoms(msgMinuscule, nomMin, nbMin);
+    }
+
+    private static void concatenerNoms (StringBuilder msg , String[]noms ,int nbNom){
+        switch (nbNom) {
+            case 1:
+                ajouterNoms(msg, noms[0]);
+            case 0:
+                break;
+            default:
+                concatenerPlusieurNoms(msg, noms, nbNom);
+                break;
+        }
+    }
+
+    private static void concatenerPlusieurNoms(StringBuilder msg, String[] noms, int nbNom) {
+        for (int i = 0; i< nbNom -1; i++){
+            ajouterNoms(msg, noms[i]);
+        }
+        if (noms[nbNom -1].toUpperCase().equals(noms[nbNom -1])){
+            msg.append(" AND ");
+        }else {
+            msg.append(" and ");
+        }
+        msg.append(nomMajuscule(noms[nbNom - 1]));
+    }
 }
+
+
--- test/welcome/WelcomeTest.java
+++ test/welcome/WelcomeTest.java
@@ -8,16 +8,16 @@ class WelcomeTest {
 
     @Test
     void welcomeNom(){
-        assertEquals(Welcome.welcome("assane"),"Hello, Assane");
-        assertEquals(Welcome.welcome("noah"),"Hello, Noah");
-        assertEquals(Welcome.welcome("jeremy"),"Hello, Jeremy");
+        assertEquals(Welcome.welcome("assane"),"Hello, Assane.");
+        assertEquals(Welcome.welcome("noah"),"Hello, Noah.");
+        assertEquals(Welcome.welcome("jeremy"),"Hello, Jeremy.");
     }
 
     @Test
     void WelcomeChaineVide(){
-        assertEquals(Welcome.welcome(""),"Hello, my friend");
-        assertEquals(Welcome.welcome(" "),"Hello, my friend");
-        assertEquals(Welcome.welcome(null),"Hello, my friend");
+        assertEquals(Welcome.welcome(""),"Hello, my friend.");
+        assertEquals(Welcome.welcome(" "),"Hello, my friend.");
+        assertEquals(Welcome.welcome(null),"Hello, my friend.");
     }
 
     @Test
@@ -29,23 +29,25 @@ class WelcomeTest {
 
     @Test
     void deux_nom(){
-        assertEquals(Welcome.welcome("assane,jeremy"),"Hello, Assane, Jeremy");
-        assertEquals(Welcome.welcome("amy,bob"),"Hello, Amy, Bob");
+        assertEquals(Welcome.welcome("assane,jeremy"),"Hello, Assane and Jeremy.");
+        assertEquals(Welcome.welcome("amy,bob"),"Hello, Amy and Bob.");
 
     }
 
     @Test
     void plusieur_nom(){
-        assertEquals(Welcome.welcome("amy,bob,jery"),"Hello, Amy, Bob, Jery");
-        assertEquals(Welcome.welcome("assane,jeremy,noah"),"Hello, Assane, Jeremy, Noah");
+        assertEquals(Welcome.welcome("amy,bob,jery"),"Hello, Amy, Bob and Jery.");
+        assertEquals(Welcome.welcome("assane,jeremy,noah"),"Hello, Assane, Jeremy and Noah.");
+        assertEquals(Welcome.welcome("assane,jeremy,noah,jery"),"Hello, Assane, Jeremy, Noah and Jery.");
 
     }
 
     @Test
     void cris_pas_cris(){
-        assertEquals(Welcome.welcome("amy,BOB,Jerry"),"Hello, Amy, Jerry. AND HELLO, BOB !");
-        assertEquals(Welcome.welcome("amy,BOB,JERRY"),"Hello, Amy. AND HELLO, BOB, JERRY !");
-        assertEquals(Welcome.welcome("AMY,BOB,Jerry"),"Hello, Jerry. AND HELLO, AMY, BOB !");
+        assertEquals(Welcome.welcome("amy,BOB,Jerry"),"Hello, Amy and Jerry. AND HELLO, BOB !");
+        assertEquals(Welcome.welcome("amy,BOB,JERRY"),"Hello, Amy. AND HELLO, BOB AND JERRY !");
+        assertEquals(Welcome.welcome("AMY,BOB,Jerry"),"Hello, Jerry. AND HELLO, AMY AND BOB !");
+        assertEquals(Welcome.welcome("AMY,BOB,Jerry,theo"),"Hello, Jerry and Theo. AND HELLO, AMY AND BOB !");
     }
 
 }
